65880a6cf17cc27b690bef9715832a5ad477825f
javier.fernandez <javier.agustin.f@gmail.com>
2023-06-03 11:30:08 -0300

    initial version

---
+++ src/main/java/com/macmillan/rsuite/sb/BatchProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.macmillan.rsuite.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BatchProcessingApplication.class, args);
+	}
+}
